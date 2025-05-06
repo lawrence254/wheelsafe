b2ddc138ebc96574ddce789b9247582e5a5f6abb
lawrence254 <lawrenslava1@gmail.com>
2025-05-06 11:43:31 +0300

    Remove unused imports

--- src/main/java/com/wheel/safe/wheelsafe/bikeshop/controller/BikeShopController.java
+++ src/main/java/com/wheel/safe/wheelsafe/bikeshop/controller/BikeShopController.java
@@ -3,11 +3,9 @@ package com.wheel.safe.wheelsafe.bikeshop.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import io.micrometer.core.ipc.http.HttpSender.Response;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 
 import java.util.List;
@@ -18,7 +16,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import com.wheel.safe.wheelsafe.bikeshop.dto.BikeShopRequest;
 import com.wheel.safe.wheelsafe.bikeshop.dto.BikeShopResponse;
-import com.wheel.safe.wheelsafe.bikeshop.entity.BikeShop;
 import com.wheel.safe.wheelsafe.bikeshop.service.BikeShopService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
